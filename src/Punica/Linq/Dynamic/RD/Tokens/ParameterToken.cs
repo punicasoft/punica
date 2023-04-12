5cb76d106a3076939aa8d66bae1102aeedb08666
Randima <randima.cse@gmail.com>
2023-04-12 11:54:21 +0530

    move to separate files

--- src/Punica/Linq/Dynamic/RD/Tokens/ParameterToken.cs
+++ src/Punica/Linq/Dynamic/RD/Tokens/ParameterToken.cs
@@ -5,33 +5,6 @@ using Punica.Linq.Dynamic.RD.Tokens.abstractions;
 
 namespace Punica.Linq.Dynamic.RD.Tokens
 {
-   
-    public class PropertyToken : IExpression
-    {
-        private readonly IExpression _expression;
-        private readonly string _propertyName;
-        private Expression _value;
-        private bool _evaluated;
-
-        public PropertyToken(IExpression expression, string propertyName)
-        {
-            _expression = expression;
-            _propertyName = propertyName;
-            _evaluated = false;
-        }
-
-        public Expression Evaluate()
-        {
-            if (!_evaluated)
-            {
-                _value = Expression.PropertyOrField(_expression.Evaluate(), _propertyName);
-                _evaluated = true;
-            }
-
-            return _value;
-        }
-    }
-
     public class ParameterToken : IExpression
     {
         private Expression _value;
---
+++ src/Punica/Linq/Dynamic/RD/Tokens/PropertyToken.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq.Expressions;
+using Punica.Linq.Dynamic.RD.Tokens.abstractions;
+
+namespace Punica.Linq.Dynamic.RD.Tokens;
+
+public class PropertyToken : IExpression
+{
+    private readonly IExpression _expression;
+    private readonly string _propertyName;
+    private Expression _value;
+    private bool _evaluated;
+
+    public PropertyToken(IExpression expression, string propertyName)
+    {
+        _expression = expression;
+        _propertyName = propertyName;
+        _evaluated = false;
+    }
+
+    public Expression Evaluate()
+    {
+        if (!_evaluated)
+        {
+            _value = Expression.PropertyOrField(_expression.Evaluate(), _propertyName);
+            _evaluated = true;
+        }
+
+        return _value;
+    }
+}
